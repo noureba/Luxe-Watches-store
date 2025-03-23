f94b5f83cf6d95c4be8e8f89ebbbc666e8e03d88
Noure DDine <94680573+noureba@users.noreply.github.com>
2025-03-23 14:09:38 +0100

    cleaning

--- Frontend/app/(pages)/layout.js
+++ Frontend/app/(pages)/layout.js
@@ -1,23 +1,18 @@
 "use client";
+import "../../styles/globals.css";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import "../styles/globals.css";
 import { Provider } from "react-redux";
 import store from "../../redux/store/store";
-import {PayPalScriptProvider} from "@paypal/react-paypal-js"
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="container m-auto ">
         <Provider store={store}>
-          <PayPalScriptProvider options={{clientId:""}}>
-
           <Header />
           {children}
           <Footer />
-          </PayPalScriptProvider>
-
         </Provider>
       </body>
     </html>
