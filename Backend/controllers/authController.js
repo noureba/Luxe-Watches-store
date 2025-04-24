0a2d1735a13c40c138571fa31b32370db26de987
Noure DDine <94680573+noureba@users.noreply.github.com>
2025-04-24 09:49:06 +0100

    cookie config

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -70,9 +70,13 @@ export const login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      // expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      // secure: process.env.NODE_ENV === "production",
+      // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
+      path: "/",
+      maxAge: 60 * 60 * 24,
     });
 
     res.status(201).json({
