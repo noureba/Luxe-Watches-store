a0ca9046ac6b00824596c70095aa0aff6987e9b1
Noure DDine <94680573+noureba@users.noreply.github.com>
2025-04-24 10:11:39 +0100

    token edit

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -70,11 +70,8 @@ export const login = async (req, res) => {
     });
     res.cookie("token", token, {
       httpOnly: true,
-      // expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-      // secure: process.env.NODE_ENV === "production",
-      // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      secure: true,
-      sameSite: "none",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       path: "/",
       maxAge: 60 * 60 * 24,
     });
